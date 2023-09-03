faf8f9fb101a51a6108584bef0fdc6245be0f6de
KirillM <mdvdvks@gmail.com>
2023-09-03 21:00:43 +0300

    HW_L3 Task 4

--- src/HW/L3/HW_L3.java
+++ src/HW/L3/HW_L3.java
@@ -7,11 +7,11 @@ public class HW_L3 {
         System.out.println("Task1. enter your number:");
         Scanner enterNumber=new Scanner(System.in);
         int a= enterNumber.nextInt();
-        System.out.println(a);
+
         if (a%2==0)
             System.out.println("Число чётное");
         else System.out.println("Число нечётное");
-
+        System.out.println("");
 
         System.out.println("Task2. enter temperature:");
         Scanner temperature=new Scanner(System.in);
@@ -22,12 +22,25 @@ public class HW_L3 {
             System.out.println("Cold");
         else
             System.out.println("Normal");
+        System.out.println("");
 
         System.out.println("Task3");
         for (int i=10; i<21; i++)
         {
-            System.out.println(i*i);
+            System.out.print(i*i +", ");
         }
+        System.out.println("");
+        System.out.println("");
+
+        System.out.println("Task4");
+        int c=7;
+        while (c<99){
+            System.out.print(c +" ");
+            c+=7;
+        }
+        System.out.println("");
+        System.out.println("");
+
 
 
 
