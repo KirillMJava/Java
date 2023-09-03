29df949846cde98d18913c8702928eeac20b775b
KirillM <mdvdvks@gmail.com>
2023-09-03 20:32:22 +0300

    HW_L3 Task 1

--- src/HW/L3/HW_L3.java
+++ src/HW/L3/HW_L3.java
@@ -1,7 +1,19 @@
 package HW.L3;
 
+import java.util.Scanner;
 public class HW_L3 {
     public static void main(String[] args) {
-        System.out.println("Hello");
+
+        System.out.println("Task1");
+        Scanner vvodChisla=new Scanner(System.in);
+        int a= vvodChisla.nextInt();
+        System.out.println(a);
+        if (a%2==0)
+            System.out.println("Число чётное");
+        else System.out.println("Число нечётное");
+        vvodChisla.close();
+
+
+
     }
 }
