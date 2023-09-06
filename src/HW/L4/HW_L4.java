c392f0d799050601ad1c3bdc346ef7e8a7dddf5b
KirillM <mdvdvks@gmail.com>
2023-09-07 00:21:54 +0300

    HW_L4_Task1

--- src/HW/L4/HW_L4.java
+++ src/HW/L4/HW_L4.java
@@ -12,7 +12,7 @@ public class HW_L4 {
         System.out.println("Введите размер массива:");
         if (sk.hasNextInt()) {
             int massDlinna = sk.nextInt();
-            System.out.println("Если хотите заполнить массив вручную, введите '1', если желаете сгенирировать заполнение, введите '2':");
+            System.out.println("Если хотите заполнить массив вручную, введите '1', если желаете сгенерировать заполнение, введите '2':");
             if (sk1.hasNextInt()) {
                 int vybor = sk1.nextInt();
                 if (vybor == 1) {
@@ -27,6 +27,16 @@ public class HW_L4 {
                         }
                     }
                     System.out.println(Arrays.toString(mass));
+
+                    System.out.println("Task1");
+                    for (int j : mass) {
+                        System.out.print(j+" ");
+                    }
+                    System.out.println();
+                    for (int i=mass.length-1; i>=0; i--){
+                        System.out.print(mass[i]+" ");
+                    }
+
                 } else if (vybor == 2) {
                         double[] mass2 = new double[massDlinna];
                         for (int i = 0; i < mass2.length; i++) {
@@ -34,12 +44,8 @@ public class HW_L4 {
                             mass2[i] = mass2[i] * 20;
                         }
                         System.out.println(Arrays.toString(mass2));
+                        System.out.println("Далее работаем с массивом, введённым вручную, запустите программу и выберите вариант ручного ввода");
                 } else System.out.println("Вы ввели неверное значение, перезапустите программу");
-
-                System.out.println("Task1");
-
-
-
             } else System.out.println("Вы ввели неверное значение, перезапустите программу");
         } else System.out.println("Вы ввели неверное значение, перезапустите программу");
 
