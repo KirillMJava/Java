422c348c7591f70ebc5f1d10f3a88c83bee10bec
KirillM <mdvdvks@gmail.com>
2023-10-12 21:39:34 +0300

    L14_Task3

---
+++ src/Lessons/L14/Tasks/Task3/IShape.java
@@ -0,0 +1,6 @@
+package Lessons.L14.Tasks.Task3;
+
+@FunctionalInterface
+public interface IShape {
+    double getSquare(int x, int y);
+}
---
+++ src/Lessons/L14/Tasks/Task3/L14_Task3.java
@@ -0,0 +1,19 @@
+package Lessons.L14.Tasks.Task3;
+
+public class L14_Task3 {
+
+    public static void main(String[] args) {
+
+    IShape square;
+    square= ((a, b) -> a*b);
+    double result = square.getSquare(10,20);
+        System.out.println(result);
+
+    IShape triangle;
+    triangle=((a, h) -> (0.5*a*h));
+    result= triangle.getSquare(10,5);
+        System.out.println(result);
+
+    }
+
+}
